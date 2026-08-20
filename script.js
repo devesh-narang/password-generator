2a4edf17bfb67b56207a5e6c3f582709d809752c
devesh-narang <deveshnarang161@gmail.com>
2026-08-20 23:14:11 +0530

    Support uppercase, numbers and symbols in generated passwords

--- script.js
+++ script.js
@@ -7,18 +7,43 @@ const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
-// Basic version for now: generates a lowercase-only password.
-// The checkbox options get wired up tomorrow.
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
-    let password = ''
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
 
-    for(let i = 0; i < length; i++) {
-        password += getRandomLower()
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+
+    if(typesCount === 0) {
+        return ''
     }
 
-    resultEl.innerText = password
-})
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
+}
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
