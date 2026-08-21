90fb7876821cfa37809a23e28f4373b49ad69fe7
devesh-narang <deveshnarang161@gmail.com>
2026-08-21 13:59:19 +0530

    Add copy to clipboard and polish result display

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+clipboardEl.addEventListener('click', () => {
+    const password = resultEl.innerText;
+  if (!password) {
+    return;
+  }
+  navigator.clipboard.writeText(password);
+    alert('Password copied to clipboard!')
+})
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
     const hasLower = lowercaseEl.checked
@@ -28,7 +37,7 @@ function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = ''
     const typesCount = lower + upper + number + symbol
     const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
-
+    
     if(typesCount === 0) {
         return ''
     }
